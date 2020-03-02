39e5db2c9db0f7a8b8fbc930b7aa1a8b1440fc0c
MrCrambo <kadirov.il.crambo@gmail.com>
2020-03-02 23:57:37 +0300

    classes second task solved

--- Coursera_1/W3_T4.cpp
+++ Coursera_1/W3_T4.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <map>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -31,9 +33,67 @@ public:
 
         return full_name;
     }
+    string GetFullNameWithHistory(int year) {
+        const string first_name = getHistoryName(FindNamesHistory(names, year));
+        const string last_name = getHistoryName(FindNamesHistory(surnames, year));
+
+        if (first_name.empty() && last_name.empty())
+            return "Incognito";
+        else if (first_name.empty())
+            return last_name + " with unknown first name";
+        else if (last_name.empty())
+            return first_name + " with unknown last name";
+
+        return first_name + " " + last_name;
+    }
+
 private:
     map<int, string> names;
     map<int, string> surnames;
+
+    vector<string> FindNamesHistory(map<int, string>& name_to_year, int year) {
+        vector<string> n;
+        string lastChangedName;
+
+        for (const auto& [key, value] : name_to_year) {
+            if (key > year) {
+                break;
+            }
+
+            if (lastChangedName != value) {
+                lastChangedName = value;
+                n.push_back(value);
+            }
+        }
+
+        return n;
+    }
+
+    string getHistoryName(vector<string> n) {
+        string result;
+        unsigned long size = names.size();
+
+        if (size == 0)
+            return result;
+
+        reverse(n.begin(), n.end());
+        result += n[0];
+
+        if (size > 1) 
+        {
+            result += " (";
+            for (int i = 1; i < size; ++i) 
+            {
+                result += n[i];
+
+                if (i < size - 1)
+                    result += ", ";
+            }
+            result += ')';
+        }
+
+        return result;
+    }
 
     int GetPreviousChangeYear(map<int, string>& history, int year)
     {
@@ -54,18 +114,33 @@ int main()
     person.ChangeFirstName(1965, "Polina");
     person.ChangeLastName(1967, "Sergeeva");
     for (int year : {1900, 1965, 1990}) {
-        cout << person.GetFullName(year) << endl;
+        cout << person.GetFullNameWithHistory(year) << endl;
     }
     
     person.ChangeFirstName(1970, "Appolinaria");
     for (int year : {1969, 1970}) {
-        cout << person.GetFullName(year) << endl;
+        cout << person.GetFullNameWithHistory(year) << endl;
     }
     
     person.ChangeLastName(1968, "Volkova");
     for (int year : {1969, 1970}) {
-        cout << person.GetFullName(year) << endl;
+        cout << person.GetFullNameWithHistory(year) << endl;
     }
+    
+    person.ChangeFirstName(1990, "Polina");
+    person.ChangeLastName(1990, "Volkova-Sergeeva");
+    cout << person.GetFullNameWithHistory(1990) << endl;
+    
+    person.ChangeFirstName(1966, "Pauline");
+    cout << person.GetFullNameWithHistory(1966) << endl;
+    
+    person.ChangeLastName(1960, "Sergeeva");
+    for (int year : {1960, 1967}) {
+        cout << person.GetFullNameWithHistory(year) << endl;
+    }
+    
+    person.ChangeLastName(1961, "Ivanova");
+    cout << person.GetFullNameWithHistory(1967) << endl;
     
     return 0;
 }
