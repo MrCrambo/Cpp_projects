6e9a9b32b2753076034150927e595a27b139dcba
MrCrambo <kadirov.il.crambo@gmail.com>
2020-03-29 18:42:58 +0300

    week 3 first task added

---
+++ Coursera_2/W3_T1.cpp
@@ -0,0 +1,17 @@
+#include "sum_reverse_sort.h" //  from the coursera task
+#include <algorithm>
+
+using namespace std;
+
+int Sum(int x, int y){
+    return x + y;
+}
+
+string Reverse(string s){
+    reverse(begin(s), end(s));
+    return s;
+}
+
+void Sort(vector<int>& nums){
+    sort(begin(nums), end(nums));
+}
