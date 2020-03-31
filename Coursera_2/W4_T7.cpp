5add59318e1ba84b0da730b472d69a3d7ae6d1a1
MrCrambo <kadirov.il.crambo@gmail.com>
2020-03-31 22:15:03 +0300

    task with prefix searching solved

---
+++ Coursera_2/W4_T7.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+
+using namespace std;
+
+template <typename RandomIt>
+pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, string prefix) 
+{
+    auto left = lower_bound(range_begin, range_end, prefix);
+    string upper_bound = prefix;
+    ++upper_bound[upper_bound.size() - 1];
+    auto right = lower_bound(range_begin, range_end, upper_bound);
+    return {left, right};
+}
+
+int main() 
+{
+    const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
+
+    const auto mo_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
+    for (auto it = mo_result.first; it != mo_result.second; ++it) {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    const auto mt_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
+    cout << (mt_result.first - begin(sorted_strings)) << " " <<
+         (mt_result.second - begin(sorted_strings)) << endl;
+
+    const auto na_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
+    cout << (na_result.first - begin(sorted_strings)) << " " <<
+         (na_result.second - begin(sorted_strings)) << endl;
+
+    return 0;
+}
