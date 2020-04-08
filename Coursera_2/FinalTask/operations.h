310f065194525567a5954f8b1e4e1a179a8661eb
MrCrambo <kadirov.il.crambo@gmail.com>
2020-04-08 23:10:37 +0300

    final task full code added

---
+++ Coursera_2/FinalTask/operations.h
@@ -0,0 +1,20 @@
+#ifndef YELLOW_BELT_OPERATIONS_H
+#define YELLOW_BELT_OPERATIONS_H
+
+enum class Comparison
+{
+    Less,
+    LessOrEqual,
+    Greater,
+    GreaterOrEqual,
+    Equal,
+    NotEqual,
+};
+
+enum class LogicalOperation
+{
+    And,
+    Or,
+};
+
+#endif //YELLOW_BELT_OPERATIONS_H
