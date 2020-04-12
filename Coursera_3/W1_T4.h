69268fb4b4637f59a2a6e9df14b94734858dbea9
MrCrambo <kadirov.il.crambo@gmail.com>
2020-04-13 00:04:49 +0300

    task with class template added

---
+++ Coursera_3/W1_T4.h
@@ -0,0 +1,98 @@
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
+template <typename T>
+class Deque
+{
+public:
+    Deque() = default;
+
+    T& Front()
+    {
+        return head.empty() ? tail.front() : head.back();
+    }
+
+    const T& Front()const
+    {
+        return head.empty() ? tail.front() : head.back();
+    }
+
+    T& Back()
+    {
+        return tail.empty() ? head.front() : tail.back();
+    }
+
+    const T& Back()const
+    {
+        return tail.empty() ? head.front() : tail.back();
+    }
+
+    T& At(size_t index)
+    {
+
+        CheckIndex(index);
+        return (*this)[index];
+    }
+    
+    const T& At(size_t index) const
+    {
+        CheckIndex(index);
+        return (*this)[index];
+    }
+
+    T& operator[](size_t index)
+    {
+        if(index < head.size())
+            return head[head.size() - index - 1];
+        else
+            return tail[index - head.size()];
+    }
+
+    const T& operator[](size_t index) const
+    {
+        if(index < head.size())
+        {
+            return head[head.size() - index - 1];
+        }
+        else
+        {
+            return tail[index - head.size()];
+        }
+    }
+
+    bool Empty()const
+    {
+        return head.empty() && tail.empty();
+    }
+
+    void Clear()
+    {
+        head.clear();
+        tail.clear();
+    }
+
+    void PushBack(const T& item)
+    {
+        tail.push_back(item);
+    }
+    void PushFront(const T& item)
+    {
+        head.push_back(item);
+    }
+
+    size_t Size()const
+    {
+        return head.size() + tail.size();
+    }
+
+private:
+    void CheckIndex(size_t i)
+    {
+        if(i >= Size())
+            throw out_of_range("Index is out of range");
+    }
+
+    vector<T> head;
+    vector<T> tail;
+};
