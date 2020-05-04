2aa52afa7f953de9a45f42fb040dbae5c6a7a47c
MrCrambo <kadirov.il.crambo@gmail.com>
2020-05-04 21:00:37 +0300

    drawing triangle with shaders

--- OpenGL/OpenGL_1/OpenGL_1/main.cpp
+++ OpenGL/OpenGL_1/OpenGL_1/main.cpp
@@ -12,12 +12,50 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <OpenGL/gl3.h>
+#include <OpenGL/glu.h>
 
-static int createVertex(const std::string& vertexShader,
-                        const std::string& fragmentShader)
+using namespace std;
+
+static unsigned int compileShader(unsigned int type, const std::string& source)
 {
-    auto program = glCreatePorgram();
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
     
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*) alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << message <<std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+    
+    return id;
+}
+
+static unsigned int createShader(const std::string& vertexShader,
+                                 const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    
+    return program;
 }
 
 int main(void)
@@ -27,8 +65,7 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
+    
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
@@ -38,11 +75,9 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    
+    glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK)
         std::cout << "Error" << std::endl;
-    
-    std::cout << glGetString(GL_VERSION) << std::endl;
     
     float positions[6] = {
         -0.5f, -0.5f, // 1st coord
@@ -63,12 +98,42 @@ int main(void)
                           sizeof(float) * 2, // stride to the next vertex - between 1st and 2nd coord i.e.
                           0 // pointer
                           );
+    
+    cout << "GLFW version                : " << glfwGetVersionString() << endl;
+    cout << "GLEW_VERSION                : " << glewGetString( GLEW_VERSION ) << endl;
+    cout << "GL_VERSION                  : " << glGetString( GL_VERSION ) << endl;
+    cout << "GL_VENDOR                   : " << glGetString( GL_VENDOR ) << endl;
+    cout << "GL_RENDERER                 : " << glGetString( GL_RENDERER ) << endl;
+    cout << "GL_SHADING_LANGUAGE_VERSION : " << glGetString( GL_SHADING_LANGUAGE_VERSION ) << endl;
+    
+    
+    std::string vertexShader =
+        "#version 120\n"
+        "\n"
+        "attribute vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+    
+    std::string fragmentShader =
+        "#version 120\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_FragColor = vec4(1.0, 0.0, 0.5, 0.5);\n"
+        "}\n";
+    
+    unsigned int shader = createShader(vertexShader, fragmentShader);
+        
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -78,7 +143,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+    glDeleteProgram(shader);
+    
     glfwTerminate();
     return 0;
 }
